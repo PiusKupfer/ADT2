5ab66d81dec3d5eb8da33ada204e704a0f6249ee
piusb <piusb@DESKTOP-8B7JO9T.speedport.ip>
2022-09-22 17:09:39 +0200

    ._.

---
+++ List/src/Arrayqueue.java
@@ -0,0 +1,80 @@
+
+public class Arrayqueue {
+	private int[] stapel;
+	private Arrayqueue stapel2;
+	private int tos;
+
+	public Arrayqueue() {
+		stapel = new int[100];
+		tos = 0;
+	}
+
+	public void ausgabe() {
+
+		int dummy = 0;
+		while (dummy != tos) {
+
+			System.out.print(stapel[dummy] + " ");
+			dummy++;
+
+		}
+		System.out.println(" ");
+
+	}
+
+	public void enqueue(int x) {
+		if (tos < 99) {
+
+			stapel[tos] = x;
+			tos++;
+
+		}
+	}
+
+	public void dequeue() {
+		for (int i = 0; i < tos; i++) {
+
+			stapel[i] = stapel[i + 1];
+
+		}
+		tos--;
+	}
+
+	public void concet(Arrayqueue stapel2) {
+		this.stapel2 = stapel2;
+
+		while (!stapel2.empty()) {
+			stapel[tos] = stapel2.front();
+			stapel2.dequeue();
+			tos++;
+
+		}
+
+	}
+
+	public boolean equal(int x) {
+
+		int tmptos = tos - 1;
+		while (tmptos > 0) {
+			if (stapel[tmptos] == x) {
+
+				return true;
+			} else {
+				tmptos--;
+			}
+		}
+		return false;
+	}
+
+	public int front() {
+		if (!empty())
+			return stapel[0];
+		else
+			return -999;
+	}
+
+	public boolean empty() {
+		return (tos == 0);
+	}
+
+}
---
+++ List/src/Main.java
@@ -0,0 +1,110 @@
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+	/*	
+		Stack stack = new Stack(1);
+		      stack.push(3);
+		      stack.push(5);
+		      stack.push(7);
+		      stack.push(9);
+		      stack.push(11);
+		      stack.push(13);
+		     
+		      stack.top();
+		      System.out.println(stack);
+		      System.out.println( stack.getGroeße());
+		      
+	  Queue queue = new Queue(1);
+	  queue.enqeue(2);
+	  queue.enqeue(4);
+	  queue.enqeue(6);
+	  queue.enqeue(8);
+	  queue.top();
+	  System.out.println(queue);
+	  */
+		
+		/*
+	  Arraystack s=new Arraystack();
+	  s.push(1);
+	  s.push(3);
+	  s.push(5);
+	  s.push(78);
+	  s.push(6);
+	  s.push(3);
+	  s.push(8);
+	  System.out.println(" ");
+	  s.copy();
+	  
+	  int[] add= {5,4,6,9,12,0};
+	  System.out.println(" ");
+	  s.add(add);*/
+		
+		Arrayqueue q = new Arrayqueue();
+		q.enqueue(2);
+		q.enqueue(9);
+		q.enqueue(78);
+		q.enqueue(34);
+		q.enqueue(5);
+		q.enqueue(88);
+	
+		q.ausgabe();
+
+
+
+		
+		Arrayqueue s = new Arrayqueue();
+		s.enqueue(1);
+		s.enqueue(2);
+		s.enqueue(3);
+		s.enqueue(4);
+		s.enqueue(5);
+		s.ausgabe();
+		
+		q.concet(s);
+		q.ausgabe();
+		
+		System.out.println( q.equal(5));
+		
+		
+	
+
+	  
+	  
+
+		
+		
+		/*Liste liste = new Liste(3);
+		liste.add(5);
+		liste.add(43);
+		liste.add(9);
+		liste.add(12);
+		liste.add(13);
+		liste.add(7);
+		
+		System.out.println(liste);
+		
+		liste.addHead(42);
+		
+		System.out.println(liste);
+		
+		
+		System.out.println(liste.counter());
+		
+		liste.firstLast();
+		
+		System.out.println("\n"+liste.find(4));
+		
+		liste.remove(7);
+		System.out.println(liste);
+		
+		/*Quicksort q= new Quicksort();
+		int[] zuSortieren= {7,2,4,8,6,1,3,5,9,3};
+		System.out.println(Arrays.toString(zuSortieren));
+		q.quicksort(zuSortieren,0,9);
+		System.out.println(Arrays.toString(zuSortieren));*/
+	}
+
+}

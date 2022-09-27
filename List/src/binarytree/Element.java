d712a461d2e2d938adca6454d6c1da58d51197e4
PiusKupfer <PiusKupfer@0-208-10>
2022-09-27 15:26:52 +0200

    fghfghfgh

--- List/src/binarytree/Element.java
+++ List/src/binarytree/Element.java
@@ -4,12 +4,14 @@ public class Element
 {
    public int value;
    public Element left, right;
-
+  public static int  depth = 0;
+  public static int maxdepth = 1;
    public Element(int n)
    {
       value = n;
       left = null;
       right = null;
+    
    }
    
    public void insert(int value) {
@@ -32,6 +34,48 @@ public class Element
 		   }
 	   }
    }
+   
+   
+	   public int depth(int depth) {
+	        
+	        if(left != null) {
+	            depth++;
+	            if(depth>maxdepth) {maxdepth++;}
+	            System.out.println("Lefttiefe: "+ depth+" Maxtiefe: "+maxdepth);
+	            left.depth(depth);
+	            depth--;
+	        }
+	        if(right != null) {
+	            depth++;
+	            if(depth>maxdepth) {maxdepth++;}
+	            System.out.println("Righttiefe: "+ depth+" Maxtiefe: "+maxdepth);
+	            right.depth(depth);
+	            depth--;}
+	            
+	        return maxdepth;
+	        }
+   
+   
+   
+   public void preOrder() {
+	  System.out.println(value + " ");
+	   if(left != null) {
+		   left.preOrder();
+	   }
+	   if(right != null) {
+		   right.preOrder();
+	   }
+   }
+   
+   
+
+			   
+   
+	   
+   
+	   
+   
+   
 
    public void show()
    {
--- List/src/binarytree/main.java
+++ List/src/binarytree/main.java
@@ -5,9 +5,17 @@ public class main {
 		public static void main(String[]args) {
 			
 			Buildtree meinBaum = new Buildtree();
+			meinBaum.root = new Element(42);
+			meinBaum.root.insert(9);
+			meinBaum.root.insert(8);
+			meinBaum.root.insert(14);
+			meinBaum.root.insert(90);
+			meinBaum.root.insert(122);
+			meinBaum.root.insert(569);
+			meinBaum.root.insert(89);
 			
-			meinBaum.root.insert(72);
-			meinBaum.root.left.right.show();
-		
+		//	meinBaum.root.left.right.show();
+			System.out.println(meinBaum.root.depth(1));
+			meinBaum.root.preOrder();
 		}
 }
